e1f7a6e65df53fa7e049b1a617e25df449028a00
armandwayoff <wayoff.armand@gmail.com>
2024-10-25 14:19:43 +0200

    Remarques + remises en page

--- illustrations/integration-02_lebesgue.py
+++ illustrations/integration-02_lebesgue.py
@@ -2,14 +2,16 @@ import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
-X = np.arange(0, 7, 0.001)
+plt.rcParams['text.usetex'] = True
+
+X = np.arange(0, 7, 0.005)
 
 g = lambda x: x**(1/2) + 2 * x**2 - np.log(x + 1) + 2
 
-K = [1, 5, 50, 100]
+K = [1, 5, 50] # 100
 
-fig, ax = plt.subplots(len(K), 1, constrained_layout=True)
-for i, k in enumerate([1, 5, 50, 100]):
+fig, ax = plt.subplots(len(K), 1, constrained_layout=True, figsize=(3, 6))
+for i, k in enumerate(K):
     f = lambda x: np.sin(k * x) * g(x)
     f = np.vectorize(f)
     Y = f(X)
@@ -17,12 +19,17 @@ for i, k in enumerate([1, 5, 50, 100]):
     negative_area = np.where(Y < 0, Y, 0)
     I = integrate.quad(f, 0, 2*np.pi)[0]
     lab = "{k}, {I:.2f}".format(k=k, I=I)
-    ax[i].set_title(r"$x \mapsto \sin($"+str(k)+r"$\cdot x) f(x)$")
+    if k == 1:
+        ax[i].set_title(r"$x \mapsto \sin(x) f(x)$")
+    else:
+        ax[i].set_title(r"$x \mapsto \sin(" + str(k) + r"\,x) f(x)$")
     ax[i].plot(X, Y, label=lab, linewidth=0.5)
     ax[i].plot(X, g(X), '--r', label=lab, linewidth=0.5)
     ax[i].plot(X, -g(X), '--r', label=lab, linewidth=0.5)
     ax[i].fill_between(X, positive_area, color='blue', alpha=0.3)
     ax[i].fill_between(X, negative_area, color='red', alpha=0.3)
     # plt.axes([0, max(X), -max(g(X)), max(g(X))]) 
+
+# plt.show()
 
 plt.savefig("integration-02_lebesgue.png", dpi=300)
